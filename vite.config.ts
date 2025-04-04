2eb2ac6f83f10a9cf64d62738a02745a390de738
Marine09 <modernlebron@gmail.com>
2025-04-04 17:54:13 +0000

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { tempo } from "tempo-devtools/dist/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tempo()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    allowedHosts:
+      process.env.TEMPO === "true"
+        ? true
+        : ["nifty-torvalds4-sfvxv.view-3.tempo-dev.app"],
+  },
+});
